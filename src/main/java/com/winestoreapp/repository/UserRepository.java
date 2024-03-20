2b799a48bf1561ce04926ac0017610aab38cb3ac
Artem Makovskyy <photomak79@gmail.com>
2024-03-20 16:23:59 +0200

    Add validation in updateRole

--- src/main/java/com/winestoreapp/repository/UserRepository.java
+++ src/main/java/com/winestoreapp/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.winestoreapp.model.RoleName;
 import com.winestoreapp.model.User;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,6 +13,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("FROM User u LEFT JOIN FETCH u.roles r WHERE u.email = :email AND u.isDeleted = FALSE "
             + "AND r.isDeleted = FALSE")
     Optional<User> findUserByEmail(String email);
+
+    @EntityGraph(attributePaths = {"roles"})
+    Optional<User> findById(Long id);
 
     Optional<User> findFirstByFirstNameAndLastName(String firstName, String lastName);
 
--- src/main/java/com/winestoreapp/service/impl/UserServiceImpl.java
+++ src/main/java/com/winestoreapp/service/impl/UserServiceImpl.java
@@ -11,6 +11,7 @@ import com.winestoreapp.model.User;
 import com.winestoreapp.repository.RoleRepository;
 import com.winestoreapp.repository.UserRepository;
 import com.winestoreapp.service.UserService;
+import java.util.List;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,6 +20,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private static final int MINIMUM_ALLOWED_NUMBER_OF_ADMIN_USERS = 1;
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
@@ -51,6 +53,17 @@ public class UserServiceImpl implements UserService {
                 () -> new EntityNotFoundException("Can't find user by id " + userId));
         final Role roleFromDb = roleRepository.findByName(RoleName.valueOf(role)).orElseThrow(
                 () -> new EntityNotFoundException("Can't find role " + role));
+        final List<Role> currentUserFromDatabaseWithAdminRoleListThatWillChange
+                = userFromDb.getRoles().stream()
+                .filter(r -> r.getName().equals(RoleName.ROLE_ADMIN))
+                .toList();
+        final List<User> usersByRoleAdminInDb = userRepository.findUsersByRole(RoleName.ROLE_ADMIN);
+        if (!currentUserFromDatabaseWithAdminRoleListThatWillChange.isEmpty()
+                && usersByRoleAdminInDb.size() == MINIMUM_ALLOWED_NUMBER_OF_ADMIN_USERS) {
+            throw new RegistrationException("You cannot change the last admin role in the "
+                    + "database. After the change, at least one user with the admin role "
+                    + "have to remain.");
+        }
         userFromDb.setRoles(Set.of(roleFromDb));
         return userMapper.toDto(userRepository.save(userFromDb));
     }
